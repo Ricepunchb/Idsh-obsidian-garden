8e59bb8fc3d9fea0530e24a12a1e307612e6709c
SuhyunBae <baeshstar@gmail.com>
2025-11-19 19:54:47 +0900

    Quartz sync: Nov 19, 2025, 7:54 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,24 +8,24 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "RicePunchb's Digital Garden",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    locale: "ko-KR",
+    baseUrl: "quartz.ricepunchb.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
-        code: "IBM Plex Mono",
+        header: "IBM Plex Sans KR",
+        body: "Noto Sans Korean",
+        code: "JetBrains Mono",
       },
       colors: {
         lightMode: {
