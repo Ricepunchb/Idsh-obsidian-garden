abb3e715289d68e28dd5bd0fb5233179fc865609
SuhyunBae <baeshstar@gmail.com>
2025-11-19 20:12:10 +0900

    Quartz sync: Nov 19, 2025, 8:12 PM

--- content/index.md
+++ content/index.md
@@ -1,6 +1,9 @@
 ---
-title: Welcome to Quartz
+title: Home
+publish: True
+
+# tags:
+#   - example-tag
 ---
 
-This is a blank Quartz installation.
-See the [documentation](https://quartz.jzhao.xyz) for how to get started.
+Hello World!!
--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "RicePunchb's Digital Garden",
+    pageTitle: "RicePunchb's Garden",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
