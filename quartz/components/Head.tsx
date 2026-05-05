878a51d3f0b04718969f5c6dcd1176d527acfb85
Suhyun_Home <baeshstar@gmail.com>
2026-05-06 01:38:49 +0900

    Head.tsx fixed for google search console

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -40,6 +40,7 @@ export default (() => {
       <head>
         <title>{title}</title>
         <meta charSet="utf-8" />
+        <meta name="google-site-verification" content="0jnke367HGngP-2XJq64rzwNFVz8bwQhe5vBm5924HA" />
         {cfg.theme.cdnCaching && cfg.theme.fontOrigin === "googleFonts" && (
           <>
             <link rel="preconnect" href="https://fonts.googleapis.com" />
